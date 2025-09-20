45cd68bf97641d613592a084a19f70c8c40afc49
Fernando Celadita <fernando.celadita@gmail.com>
2025-09-20 03:13:29 -0500

    Add google-services.json with SHA1 and Firebase configuration dependencies

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -2,6 +2,7 @@ plugins {
     alias(libs.plugins.android.application)
     alias(libs.plugins.kotlin.android)
     alias(libs.plugins.kotlin.compose)
+    alias(libs.plugins.google.services)
 }
 
 android {
@@ -40,6 +41,43 @@ android {
 }
 
 dependencies {
+    //Te permite declarar un NavHost con todas tus rutas y moverte entre pantallas usando un NavController.
+    implementation(libs.androidx.navigation.compose)
+
+    implementation("androidx.compose.material:material-icons-extended")
+
+    // Importa el BoM para Firebase para asegurar compatibilidad de versiones
+    implementation(platform("com.google.firebase:firebase-bom:34.3.0"))
+
+    // TODO: Add the dependencies for any other Firebase products you want to use
+    // See https://firebase.google.com/docs/android/setup#available-libraries
+    // For example, add the dependencies for Firebase Authentication and Cloud Firestore
+
+    // Firebase Authentication
+    implementation("com.google.firebase:firebase-auth")
+
+    // Cloud Firestore
+    implementation("com.google.firebase:firebase-firestore")
+
+    // Cloud Functions (para invocar tus funciones desde la app)
+    //implementation("com.google.firebase:firebase-functions")
+
+    // Firebase Cloud Messaging (FCM)
+    //implementation("com.google.firebase:firebase-messaging")
+
+    // Cloud Storage para Firebase
+    //implementation("com.google.firebase:firebase-storage")
+
+    // Firebase Analytics (para tracking de comportamiento y recomendaciones)
+    implementation("com.google.firebase:firebase-analytics")
+
+    // Google Sign-In SDK (muy útil para "Autenticación unificada" si incluyes Google)
+    // versión estable (23/07/25): https://developers.google.com/android/guides/releases?hl=es-419#july_23_2025
+    implementation("com.google.android.gms:play-services-auth:21.4.0")
+
+    // Firebase UI (opcional, pero puede acelerar el desarrollo de UI de Auth/Firestore)
+    // implementation("com.firebaseui:firebase-ui-auth:8.0.2")
+    // implementation("com.firebaseui:firebase-ui-firestore:8.0.2")
 
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.lifecycle.runtime.ktx)
